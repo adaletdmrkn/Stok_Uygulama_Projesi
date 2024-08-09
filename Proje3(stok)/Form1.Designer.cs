9313e5d34f32ed5491806870480ca5f2f0dd9207
Adalet DEMİRKAN <demirkanadalet@gmail.com>
2024-08-09 11:58:42 +0300

    Add project files.

---
+++ Proje3(stok)/Form1.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace Proje3_stok_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Panel panel1;
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnGörüntüle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMarka = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbUrunTur = new System.Windows.Forms.ComboBox();
+            this.urunTuruBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.urunstokDataSet = new Proje3_stok_.urunstokDataSet();
+            this.urunTuruTableAdapter = new Proje3_stok_.urunstokDataSetTableAdapters.urunTuruTableAdapter();
+            this.cbRenk = new System.Windows.Forms.ComboBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.urunTuruBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunstokDataSet)).BeginInit();
+            panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.Control;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.ForeColor = System.Drawing.SystemColors.Control;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1167, 459);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "id";
+            this.columnHeader1.Width = 148;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Ad";
+            this.columnHeader2.Width = 195;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Marka";
+            this.columnHeader3.Width = 164;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Tür";
+            this.columnHeader4.Width = 143;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Renk";
+            this.columnHeader5.Width = 161;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Adet";
+            this.columnHeader6.Width = 147;
+            // 
+            // btnGörüntüle
+            // 
+            this.btnGörüntüle.BackColor = System.Drawing.Color.Green;
+            this.btnGörüntüle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGörüntüle.Location = new System.Drawing.Point(23, 26);
+            this.btnGörüntüle.Name = "btnGörüntüle";
+            this.btnGörüntüle.Size = new System.Drawing.Size(165, 45);
+            this.btnGörüntüle.TabIndex = 1;
+            this.btnGörüntüle.Text = "Görüntüle";
+            this.btnGörüntüle.UseVisualStyleBackColor = false;
+            this.btnGörüntüle.Click += new System.EventHandler(this.btnGörüntüle_Click);
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.BackColor = System.Drawing.Color.Green;
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(352, 26);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(165, 45);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = false;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(114, 23);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(234, 22);
+            this.txtAd.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(19, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ad:";
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.Location = new System.Drawing.Point(114, 64);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(234, 22);
+            this.txtMarka.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(19, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Marka:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(19, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Tür:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(368, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Renk:";
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(463, 66);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(234, 22);
+            this.txtAdet.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(368, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Adet:";
+            // 
+            // cbUrunTur
+            // 
+            this.cbUrunTur.DataSource = this.urunTuruBindingSource;
+            this.cbUrunTur.DisplayMember = "urunTuru";
+            this.cbUrunTur.FormattingEnabled = true;
+            this.cbUrunTur.Location = new System.Drawing.Point(114, 110);
+            this.cbUrunTur.Name = "cbUrunTur";
+            this.cbUrunTur.Size = new System.Drawing.Size(234, 24);
+            this.cbUrunTur.TabIndex = 15;
+            this.cbUrunTur.ValueMember = "id";
+            // 
+            // urunTuruBindingSource
+            // 
+            this.urunTuruBindingSource.DataMember = "urunTuru";
+            this.urunTuruBindingSource.DataSource = this.urunstokDataSet;
+            // 
+            // urunstokDataSet
+            // 
+            this.urunstokDataSet.DataSetName = "urunstokDataSet";
+            this.urunstokDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // urunTuruTableAdapter
+            // 
+            this.urunTuruTableAdapter.ClearBeforeFill = true;
+            // 
+            // cbRenk
+            // 
+            this.cbRenk.DataSource = this.urunTuruBindingSource;
+            this.cbRenk.DisplayMember = "urunTuru";
+            this.cbRenk.FormattingEnabled = true;
+            this.cbRenk.Location = new System.Drawing.Point(463, 21);
+            this.cbRenk.Name = "cbRenk";
+            this.cbRenk.Size = new System.Drawing.Size(234, 24);
+            this.cbRenk.TabIndex = 16;
+            this.cbRenk.ValueMember = "id";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            panel1.Controls.Add(this.pictureBox1);
+            panel1.Controls.Add(this.panel2);
+            panel1.Controls.Add(this.cbRenk);
+            panel1.Controls.Add(this.label2);
+            panel1.Controls.Add(this.cbUrunTur);
+            panel1.Controls.Add(this.txtAd);
+            panel1.Controls.Add(this.txtAdet);
+            panel1.Controls.Add(this.label3);
+            panel1.Controls.Add(this.label6);
+            panel1.Controls.Add(this.txtMarka);
+            panel1.Controls.Add(this.label4);
+            panel1.Controls.Add(this.label5);
+            panel1.Cursor = System.Windows.Forms.Cursors.Default;
+            panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            panel1.ForeColor = System.Drawing.Color.Black;
+            panel1.ImeMode = System.Windows.Forms.ImeMode.On;
+            panel1.Location = new System.Drawing.Point(0, 459);
+            panel1.Name = "panel1";
+            panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            panel1.Size = new System.Drawing.Size(1167, 231);
+            panel1.TabIndex = 17;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnSil);
+            this.panel2.Controls.Add(this.btnKaydet);
+            this.panel2.Controls.Add(this.btnGörüntüle);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 148);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1163, 79);
+            this.panel2.TabIndex = 17;
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.Color.Green;
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(678, 26);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(165, 45);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.listView1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1167, 459);
+            this.panel3.TabIndex = 18;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.panel3);
+            this.panel4.Controls.Add(panel1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1167, 690);
+            this.panel4.TabIndex = 19;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Proje3_stok_.Properties.Resources.unnamed;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Location = new System.Drawing.Point(946, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 139);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1167, 690);
+            this.Controls.Add(this.panel4);
+            this.Name = "Form1";
+            this.Text = "DENTAŞ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.urunTuruBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunstokDataSet)).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button btnGörüntüle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMarka;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbUrunTur;
+        private urunstokDataSet urunstokDataSet;
+        private System.Windows.Forms.BindingSource urunTuruBindingSource;
+        private urunstokDataSetTableAdapters.urunTuruTableAdapter urunTuruTableAdapter;
+        private System.Windows.Forms.ComboBox cbRenk;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Proje3(stok)/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Proje3_stok_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglan = new SqlConnection("data source=DESKTOP-G81T251\\SQLEXPRESS;Initial Catalog=urunstok;Integrated Security=true");
+        //sql arasındaki bağlantıyı kuruyor.
+        private void verilerigörüntüle()
+        {
+            listView1.Items.Clear();
+            baglan.Open();  //bağlantıyı aç
+            SqlCommand komut = new SqlCommand("select * FROM [urunstok].[dbo].[View_urun] order by id DESC", baglan);   //"urun" isimle tabloya bağlanmasını sağlıyor.    
+            SqlDataReader oku = komut.ExecuteReader();  //oku komutu atanıyor.
+            while (oku.Read()) //okuma döngüsü başlatılıyor.
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["id"].ToString();
+                ekle.SubItems.Add(oku["Ad"].ToString());                 //ürünleri okuması için yazılmış kod
+                ekle.SubItems.Add(oku["Marka"].ToString());
+                ekle.SubItems.Add(oku["urunTuru"].ToString());
+                ekle.SubItems.Add(oku["Renk"].ToString());
+                ekle.SubItems.Add(oku["Adet"].ToString());
+                ekle.SubItems.Add(oku["isActive"].ToString());
+                listView1.Items.Add(ekle);
+            }
+            baglan.Close();  //bağlantıyı kapat
+        }
+        private void btnGörüntüle_Click(object sender, EventArgs e)
+        {
+            verilerigörüntüle();  //görüntüle butonuna basıldığında sqle girilen ürünleri görüntülüyor.
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("insert into urun(Ad,Marka,TurId,RenkId,Adet,isActive) values ('" + txtAd.Text.ToString() + "','" + txtMarka.Text.ToString() + "','" + cbUrunTur.SelectedValue.ToString() + "','" + cbRenk.SelectedValue.ToString() + "','" + txtAdet.Text.ToString() + "',1)", baglan);
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigörüntüle();
+        }
+        private void cbTurDoldur()
+        {
+            string connectionString = "data source=DESKTOP-G81T251\\SQLEXPRESS;Initial Catalog=urunstok;Integrated Security=true"; // Update with your connection string
+            string query = "SELECT * FROM renk"; // Your SQL query
+
+            // Create a DataTable to hold the data
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // Create a SqlDataAdapter
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+
+                try
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Fill the DataTable
+                    dataAdapter.Fill(dataTable);
+
+                    // You can now work with your DataTable
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        Console.WriteLine(string.Join(", ", row.ItemArray));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+            }
+
+            cbRenk.DataSource = dataTable;
+            cbRenk.ValueMember = dataTable.Columns["id"].ToString();
+            cbRenk.DisplayMember = dataTable.Columns["Renk"].ToString();          
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'urunstokDataSet.urunTuru' table. You can move, or remove it, as needed.
+            this.urunTuruTableAdapter.Fill(this.urunstokDataSet.urunTuru);
+            cbTurDoldur();
+        }
+
+        int id = 0;
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("delete from urun where id=(" + id + ")", baglan);
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            verilerigörüntüle();
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            id = int.Parse(listView1.SelectedItems[0].SubItems[0].Text);
+            txtAd.Text = listView1.SelectedItems[0].SubItems[1].Text;
+            txtMarka.Text = listView1.SelectedItems[0].SubItems[2].Text;
+            cbUrunTur.Text = listView1.SelectedItems[0].SubItems[3].Text;
+            cbRenk.Text = listView1.SelectedItems[0].SubItems[4].Text;
+            txtAdet.Text = listView1.SelectedItems[0].SubItems[5].Text;
+
+        }
+
+        
+    }
+}
